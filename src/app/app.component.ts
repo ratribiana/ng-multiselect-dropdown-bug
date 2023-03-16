cadca824b3603ac96825266ed56b551e115f20e9
Rob <robert.tribiana@eclaro.com>
2023-03-17 02:53:56 +0800

    first commit reproduce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IDropdownSettings } from "ng-multiselect-dropdown"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ng-multiselect-dropdown';
+export class AppComponent implements OnInit {
+  title = "ng-multiselect-dropdown";
+  countries: Array<any> = [];
+  selCountries = [
+    {
+      item_id: 1,
+      item_text: "Philippines",
+      image: "https://flagcdn.com/16x12/ph.png"
+    },
+    {
+      item_id: 5,
+      item_text: "United States",
+      image: "https://flagcdn.com/16x12/us.png"
+    }
+  ];
+  dropdownSettings: IDropdownSettings;
+
+  ngOnInit() {
+    this.countries = [
+      {
+        item_id: 1,
+        item_text: "Philippines",
+        image: "https://flagcdn.com/16x12/ph.png"
+      },
+      {
+        item_id: 2,
+        item_text: "Spain",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/br.jpg"
+      },
+      {
+        item_id: 3,
+        item_text: "United Kingdom",
+        image:
+          "http://www.sciencekids.co.nz/images/pictures/flags96/uk.jpg"
+      },
+      {
+        item_id: 4,
+        item_text: "Canada",
+        image:
+          "http://www.sciencekids.co.nz/images/pictures/flags96/ca.jpg",
+        isDisabled: true
+      },
+      {
+        item_id: 5,
+        item_text: "United States",
+        image: "https://flagcdn.com/16x12/us.png"
+      },
+      {
+        item_id: 6,
+        item_text: "Brazil",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/Brazil.jpg"
+      },
+      {
+        item_id: 7,
+        item_text: "Barbados",
+        image:
+          "http://www.sciencekids.co.nz/images/pictures/flags96/Barbados.jpg"
+      },
+      {
+        item_id: 8,
+        item_text: "Mexico",
+        image: "http://www.sciencekids.co.nz/images/pictures/flags96/Mexico.jpg"
+      }
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: "item_id",
+      textField: "item_text",
+      selectAllText: "Select All",
+      unSelectAllText: "UnSelect All",
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  get getItems() {
+    return this.countries.reduce((acc, curr) => {
+      acc[curr.item_id] = curr;
+      console.log(acc)
+      return acc;
+    }, {});
+  }
+
+  onItemSelect(item: any) {
+    console.log("onItemSelect", item);
+  }
+  onSelectAll(items: any) {
+    console.log("onSelectAll", items);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMultiSelectDropDownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
